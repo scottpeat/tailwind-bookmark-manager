64020eda2ef4868ea5cf114439490d4677ffdfe6
Scott Peat <scottpeatweb@gmail.com>
2025-07-26 21:03:57 +0100

    Completed JavaSrcipt code for functioning tabs and now moving onto Download boxes section

--- js/script.js
+++ js/script.js
@@ -19,6 +19,9 @@ function onTabClick(e) {
 
   // Activate a new tab and panel based on the target
   e.target.classList.add('border-softRed', 'border-b-4');
-  const classString = e.target.getAttribute('data-target')
-  
+  const classString = e.target.getAttribute('data-target');
+  document
+    .getElementById('panels')
+    .getElementsByClassName(classString)[0]
+    .classList.remove('hidden');
 }
